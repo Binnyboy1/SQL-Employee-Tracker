792bbd777f8e63a2864dd0da2436d016b441f14b
Binny <ibinstock@outlook.com>
2022-11-08 18:42:05 -0800

    sql debug fix

--- db/schema.sql
+++ db/schema.sql
@@ -4,39 +4,39 @@ CREATE DATABASE members_db;
 USE members_db;
 
 CREATE TABLE department (
-  id INT NOT NULL,
-  name VARCHAR(30) NOT NULL,
+    id INT NOT NULL,
+    name VARCHAR(30) NOT NULL,
 
-  PRIMARY KEY (id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INT NOT NULL,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT NOT NULL,
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
 
-  FOREIGN KEY (department_id)
-  REFERENCES department(id)
-  ON DELETE SET NULL
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET null,
 
-  PRIMARY KEY (id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT NOT NULL,
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
 
-  FOREIGN KEY (role_id)
-  REFERENCES role(id)
-  ON DELETE SET NULL
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET null,
 
-  PRIMARY KEY (id)
-
-  FOREIGN KEY (manager_id)
-  REFERENCES employee(id)
-  ON DELETE SET NULL
+    PRIMARY KEY (id),
+    
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 );
