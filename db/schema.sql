021686983fa2ea8e30571a78dd627cc37e3d63f8
Binny <ibinstock@outlook.com>
2022-11-08 18:01:03 -0800

    added foreign primary keys to sql database

--- db/schema.sql
+++ db/schema.sql
@@ -5,14 +5,22 @@ USE members_db;
 
 CREATE TABLE department (
   id INT NOT NULL,
-  name VARCHAR(30) NOT NULL
+  name VARCHAR(30) NOT NULL,
+
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
   id INT NOT NULL,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT NOT NULL
+  department_id INT NOT NULL,
+
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
@@ -20,5 +28,15 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT NOT NULL
+  manager_id INT NOT NULL,
+
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL
+
+  PRIMARY KEY (id)
+
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
 );
