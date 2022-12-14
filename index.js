e7d4e5097473d2295daba41f2f4fda81ba533827
Binny <ibinstock@outlook.com>
2022-12-13 22:23:00 -0800

    finished 'Add Role' functionality

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ function init() {
                 ON ${key_position_2} = ${key_2}
                 `;
                 */
+                // learned from https://www.programiz.com/sql/inner-join 
                 const key_position = "A.role_id";
                 const key = "B.id";
                 const sql = `
@@ -107,6 +108,163 @@ function init() {
                     }
                 });
 
+            } else if (answers.start === "Add Employee") {
+
+                db.query("SELECT * FROM role_table", (err, data) => {
+                    if (err) {
+                        console.log(err.message);
+                        return;
+                    } else {
+                        const data_role = data.map(function(item){return item.title;});
+                        // learned from https://stackoverflow.com/questions/7172277/how-to-select-a-particular-field-from-javascript-array 
+
+                        var dict = {};
+                        for (var i = 0; i < data.length; i++) {
+                            dict[data[i].title] = data[i].id;
+                        }
+
+                        db.query("SELECT CONCAT(A.first_name, ' ', A.last_name) AS full_name, A.id FROM employee_table AS A", (err, data2) => {
+                            if (err) {
+                                console.log(err.message);
+                                return;
+                            } else {
+                                const data_manager = data2.map(function(item){return item.full_name;});
+
+                                var dict2 = {};
+                                for (var i = 0; i < data2.length; i++) {
+                                    dict2[data2[i].full_name] = data2[i].id;
+                                }
+
+                                inquirer
+                                    .prompt([
+                                        {
+                                            type: 'input',
+                                            message: `What is the employee's first name?`,
+                                            name: 'fName'
+                                        },
+                                        {
+                                            type: 'input',
+                                            message: `What is the employee's last name?`,
+                                            name: 'lName'
+                                        },
+                                        {
+                                            type: 'list',
+                                            message: `What is the employee's role?`,
+                                            choices: data_role,
+                                            name: 'role'
+                                        }
+                                        ,
+                                        {
+                                            type: 'list',
+                                            message: `Who is the employee's manager?`,
+                                            choices: [ "None", ...data_manager ],
+                                            name: 'manager'
+                                        }
+                                    ])
+                                    .then((answers2) => {
+                                        
+                                        var manager_val = `${dict2[answers2.manager]}`;
+                                        if (dict2[answers2.manager] === undefined) {
+                                            manager_val = null;
+                                        }
+
+                                        const sql = `
+                                        INSERT INTO employee_table (first_name, last_name, role_id, manager_id)
+                                        VALUES ("${answers2.fName}", "${answers2.lName}", "${dict[answers2.role]}", ${manager_val})
+                                        `;
+                                        
+                                        db.query(sql, (err, data) => {
+                                            if (err) {
+                                                console.log(err.message);
+                                                return;
+                                            } else {
+                                                console.log("SUCCESS!");
+                                            }
+                                        });
+
+                                    })
+                                    .catch((error) => {
+                                        if (error.isTtyError) {
+                                            console.error("Prompt couldn't be rendered in the current environment");
+                                        } else {
+                                            console.error("Something else went wrong");
+                                        }
+                                    });
+
+                            }
+                        });
+                    }
+                });
+
+                /*
+                db.query("SELECT * from role_table", (err, data) => {
+                    if (err) {
+                        console.log(err.message);
+                        return;
+                    } else {
+                        const data_dep = data.map(function(item){return item.dep_name;});
+                        // learned from https://stackoverflow.com/questions/7172277/how-to-select-a-particular-field-from-javascript-array 
+
+                        inquirer
+                            .prompt([
+                                {
+                                    type: 'input',
+                                    message: `What is the employee's first name?`,
+                                    name: 'fName'
+                                },
+                                {
+                                    type: 'input',
+                                    message: `What is the employee's last name?`,
+                                    name: 'lName'
+                                },
+                                {
+                                    type: 'input',
+                                    message: `What is the employee's role?`,
+                                    choices: data_dep,
+                                    name: 'role'
+                                }
+                            ])
+                            .then((answers) => {
+                            
+                            })
+                            .catch((error) => {
+                                if (error.isTtyError) {
+                                    console.error("Prompt couldn't be rendered in the current environment");
+                                } else {
+                                    console.error("Something else went wrong");
+                                }
+                            });
+
+
+
+                    }
+                });
+
+                const key_position = "A.role_id";
+                const key = "B.id";
+                const sql = `
+                INSERT INTO employee_table (id, first_name, last_name, role_id, manager_id)
+                VALUES (1, "apple");
+                `;
+
+                db.query(sql, (err, data) => {
+                    if (err) {
+                        console.log(err.message);
+                        return;
+                    } else {
+                        console.log('\n');
+                        console.table(data);
+                        init();
+                    }
+                });
+                */
+
+            } else if (answers.start === "Add Role") {
+
+            } else if (answers.start === "Add Department") {
+
+            } else if (answers.start === "Update Employee Role") {
+            
             } else if (answers.start !== "Quit") {
                 init();
             }
@@ -114,9 +272,9 @@ function init() {
         })
         .catch((error) => {
             if (error.isTtyError) {
-                // Prompt couldn't be rendered in the current environment
+                console.error("Prompt couldn't be rendered in the current environment");
             } else {
-                // Something else went wrong
+                console.error("Something else went wrong");
             }
         });
 }
