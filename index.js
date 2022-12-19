91aaf2ae508e50d28da1113b2405a21a6dfa8aaa
Binny <ibinstock@outlook.com>
2022-12-19 05:56:48 -0800

    adding what I have to the repo

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function init() {
                 SELECT role_table.id, role_table.title, role_table.salary, department_table.dep_name, ${fill_id}
                 FROM department_table 
                 JOIN role_table
-                ON ${fill_id} = ${key_id}
+                ON ${key_id} = ${fill_id}
                 `;
 
                 db.query(sql, (err, data) => {
@@ -152,8 +152,7 @@ function init() {
                                             message: `What is the employee's role?`,
                                             choices: data_role,
                                             name: 'role'
-                                        }
-                                        ,
+                                        },
                                         {
                                             type: 'list',
                                             message: `Who is the employee's manager?`,
@@ -178,7 +177,8 @@ function init() {
                                                 console.log(err.message);
                                                 return;
                                             } else {
-                                                console.log("SUCCESS!");
+                                                console.log(`Added ${answers2.fName} ${answers2.lName} to the database`);
+                                                init();
                                             }
                                         });
 
@@ -196,36 +196,57 @@ function init() {
                     }
                 });
 
-                /*
-                db.query("SELECT * from role_table", (err, data) => {
+            } else if (answers.start === "Add Role") {
+
+                db.query("SELECT * FROM department_table", (err, data) => {
                     if (err) {
                         console.log(err.message);
                         return;
                     } else {
                         const data_dep = data.map(function(item){return item.dep_name;});
-                        // learned from https://stackoverflow.com/questions/7172277/how-to-select-a-particular-field-from-javascript-array 
+
+                        var dict = {};
+                        for (var i = 0; i < data.length; i++) {
+                            dict[data[i].dep_name] = data[i].id;
+                        }
+                        console.log(dict);
 
                         inquirer
                             .prompt([
                                 {
                                     type: 'input',
-                                    message: `What is the employee's first name?`,
-                                    name: 'fName'
-                                },
-                                {
-                                    type: 'input',
-                                    message: `What is the employee's last name?`,
-                                    name: 'lName'
-                                },
-                                {
-                                    type: 'input',
-                                    message: `What is the employee's role?`,
-                                    choices: data_dep,
+                                    message: `What is the name of the role?`,
                                     name: 'role'
+                                },
+                                {
+                                    type: 'input',
+                                    message: `What is the salary of the role?`,
+                                    name: 'salary'
+                                },
+                                {
+                                    type: 'list',
+                                    message: `What department does the role belong to?`,
+                                    choices: data_dep,
+                                    name: 'dep'
                                 }
                             ])
                             .then((answers) => {
-                            
+
+                                const sql = `
+                                INSERT INTO role_table (title, salary, department_id)
+                                VALUES ("${answers.role}", "${answers.salary}", "${dict[answers.dep]}")
+                                `;
+                                
+                                db.query(sql, (err, data2) => {
+                                    if (err) {
+                                        console.log(err.message);
+                                        return;
+                                    } else {
+                                        console.log(`Added ${answers.role} to the database`);
+                                        init();
+                                    }
+                                });
+
                             })
                             .catch((error) => {
                                 if (error.isTtyError) {
@@ -234,37 +255,13 @@ function init() {
                                     console.error("Something else went wrong");
                                 }
                             });
-
-
-
                     }
                 });
-
-                const key_position = "A.role_id";
-                const key = "B.id";
-                const sql = `
-                INSERT INTO employee_table (id, first_name, last_name, role_id, manager_id)
-                VALUES (1, "apple");
-                `;
-
-                db.query(sql, (err, data) => {
-                    if (err) {
-                        console.log(err.message);
-                        return;
-                    } else {
-                        console.log('\n');
-                        console.table(data);
-                        init();
-                    }
-                });
-                */
-
-            } else if (answers.start === "Add Role") {
 
             } else if (answers.start === "Add Department") {
-
+                init();
             } else if (answers.start === "Update Employee Role") {
-            
+                init();
             } else if (answers.start !== "Quit") {
                 init();
             }
